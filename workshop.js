2c779fa5c314b5d05a1551d75072f5bbb064f727
Julian Bias <julian_bias@yahoo.com>
2024-07-30 20:50:31 -0500

    block 07 vault msg

---
+++ workshop.js
@@ -0,0 +1,19 @@
+// I will create my message string first and test it
+// next i will start creating my equations to give desired results
+// then test all equations for accuracy
+// then create an alert box displaying the answers as the vault code
+
+let msg =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination.";
+console.log(msg);
+
+let num1 = 2 + 8;
+let num2 = 60 - 20;
+let num3 = 40 - 1;
+let code = `${num1} - ${num2} - ${num3}`;
+console.log(code);
+alert(code);
+
+// honestly, i kinda just enter the "flow state" while coding. to accomplish my previous
+// goal i defined equations to give me each value for my vault
+// then added a final string displaying the vault password
